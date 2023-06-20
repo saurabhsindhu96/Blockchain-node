7e0fdbb6ce4182032959b1f92649395468f3c35e
Saurabh Sindhu <saurabhsindhu96@gmail.com>
2023-06-20 20:18:04 +0530

    Created a genesis block

--- src/models/blockchain.rs
+++ src/models/blockchain.rs
@@ -13,4 +13,25 @@ pub struct Blockchain {
   // Minimum amount of work required to validate a block.
   pub difficulty: usize
 }
-impl Blockchain {}
+impl Blockchain {
+    pub fn new(difficulty: usize) -> Self {
+      // First block in the chain.
+      let mut genesis_block = Block {
+         index: 0,
+         timestamp: Utc::now().timestamp_millis() as u64,
+         proof_of_work: u64::default(),
+         previous_hash: String::default(),
+         hash: String::default()
+      };
+      // Create chain starting from the genesis chain.
+      let mut chain = Vec::new();
+      chain.push(genesis_block.clone());
+      // Create a blockchain Instance.
+      let blockchain = Blockchain {
+         genesis_block,
+         chain,
+         difficulty
+      };
+      blockchain
+    }
+ }
