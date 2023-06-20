5a1b4ad898745014f808c05e62a947d43eedec3c
Saurabh Sindhu <saurabhsindhu96@gmail.com>
2023-06-20 20:16:08 +0530

    Block types

--- src/models/block.rs
+++ src/models/block.rs
@@ -2,3 +2,20 @@ use super::blockchain::Blockchain;
 use chrono::prelude::*;
 use sha2::{Sha256, Digest};
 use serde::{Deserialize, Serialize};
+
+// `Block`, A struct that represents a block in a Blockchain.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Block {
+   // The index in which the current block is stored.
+   pub index: u64,
+   // The time the current block is created.
+   pub timestamp: u64,
+
+   // The block's proof of work.
+   pub proof_of_work: u64,
+   // The previous block hash.
+   pub previous_hash: String,
+   // The current block hash.
+   pub hash: String
+}
+impl Block {}
--- src/models/blockchain.rs
+++ src/models/blockchain.rs
@@ -1,3 +1,16 @@
 use chrono::prelude::*;
 // Internal module
 use super::block::Block;
+
+type Blocks = Vec<Block>;
+// `Blockchain` A struct that represents the blockchain.
+#[derive(Debug)]
+pub struct Blockchain {
+  // The first block to be added to the chain.
+  pub genesis_block: Block,
+  // The storage for blocks.
+  pub chain: Blocks,
+  // Minimum amount of work required to validate a block.
+  pub difficulty: usize
+}
+impl Blockchain {}
